b1a6b2d44a87ab745ae1dca0646aa6490c292fbe
mateusz.kowalski <mkmotywacja@gmail.com>
2024-12-08 07:13:42 +0100

    finish clock app

--- src/projects/002-Clock/clock.tsx
+++ src/projects/002-Clock/clock.tsx
@@ -1,29 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import { cn } from 'utils/cn';
 import clsx from 'clsx';
 
 export function Clock() {
-  const [hoursAsPercentage, setHoursAsPercentage] = useState(0);
-  const [minutesAsPercentage, setMinutesAsPercentage] = useState(0);
-  const [secondsAsPercentage, setSecondsAsPercentage] = useState(0);
+  const [hoursRatio, setHoursRatio] = useState(0);
+  const [minutesRatio, setMinutesRatio] = useState(0);
+  const [secondsRatio, setSecondsRatio] = useState(0);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const intervalId = setInterval(() => {
       const date = new Date();
       const hours = date.getHours();
       const minutes = date.getMinutes();
       const seconds = date.getSeconds();
 
-      console.log('minutes', minutes);
-      console.log('(minutes / 60) * 360', (minutes / 60) * 360);
+      const hourRatio = 360 / 12;
+      const minuteRatio = 360 / 60;
+      const secondsRatio = 360 / 60;
 
-      const hoursPercentage = (hours / 24) * 360;
-      const minutesPercentage = (minutes / 60) * 360;
-      const secondsPercentage = (seconds / 60) * 360;
-
-      setHoursAsPercentage(hoursPercentage);
-      setMinutesAsPercentage(minutesPercentage);
-      setSecondsAsPercentage(secondsPercentage);
+      setHoursRatio(hourRatio * hours);
+      setMinutesRatio(minuteRatio * minutes);
+      setSecondsRatio(secondsRatio * seconds);
     }, 1000);
 
     return () => {
@@ -31,25 +28,27 @@ export function Clock() {
     };
   }, []);
 
-  //   0 6 12 18 24 36
-
   return (
     <div className='w-full h-screen bg-gradient-to-r from-blue-700 to-blue-400'>
       <div className='flex justify-center items-center h-screen'>
-        <div className='w-52 h-52 border-8 border-white border-solid border-rad rounded-full relative'>
+        <div className='w-52 h-52 border-8 border-white border-solid border-rad rounded-full relative '>
           <div
             className={clsx(
-              `bg-black h-1 absolute w-4/12 left-1/2 top-1/2 origin-left ] `
+              'bg-black h-1 absolute w-4/12 left-1/2 top-1/2 origin-[0_0] transition-all'
             )}
-            style={{ transform: `rotate(${18}deg)` }}
+            style={{ transform: `rotate(${hoursRatio - 90}deg)` }}
           />
           <div
-            className={`bg-black h-1 absolute w-5/12 left-1/2 top-1/2 origin-left `}
-            style={{ transform: `rotate(${0}deg)` }}
+            className={clsx(
+              'bg-black h-1 absolute w-5/12 left-1/2 top-1/2 origin-[0_0] transition-all'
+            )}
+            style={{ transform: `rotate(${minutesRatio - 90}deg)` }}
           />
           <div
-            className={`bg-black h-1 absolute w-6/12 left-1/2 top-1/2 origin-left `}
-            style={{ transform: `rotate(${0}deg)` }}
+            className={clsx(
+              'bg-black h-1 absolute w-6/12 left-1/2 top-1/2 origin-[0_0] transition-all'
+            )}
+            style={{ transform: `rotate(${secondsRatio - 90}deg)` }}
           />
         </div>
       </div>
